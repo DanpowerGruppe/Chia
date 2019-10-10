e66f2e22ca5a19fa75b76f79bbfe1e4123c47ee1
tforkmann <tforkmann@gmail.com>
2019-10-10 21:52:50 +0200

    print variables

--- build.fsx
+++ build.fsx
@@ -209,6 +209,7 @@ let isNullOrWhiteSpace = String.IsNullOrWhiteSpace
 // Workaround for https://github.com/fsharp/FAKE/issues/2242
 let pushPackage _ =
     let nugetCmd fileName key = sprintf "nuget push %s -k %s -s nuget.org" fileName key
+    printfn "Variables %A" (Environment.environVars())
     let key = Environment.environVarOrFail "nugetKey"
         // match getBuildParam "nugetkey" with
         // | s when not (isNullOrWhiteSpace s) -> s
