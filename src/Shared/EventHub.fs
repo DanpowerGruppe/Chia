c83eb60a1555595812095bbbd75a4577f7fffb8d
tforkmann <tforkmann@gmail.com>
2019-12-09 14:39:29 +0100

    Bumping version to 0.2.6

--- src/Shared/EventHub.fs
+++ src/Shared/EventHub.fs
@@ -17,18 +17,16 @@ open Microsoft.Azure.EventHubs
 module EventHub =
     let getEventHubClient eventHubConnectionString = EventHubClient.CreateFromConnectionString(eventHubConnectionString)
 
-    let pushSingleEvent (record: 'a,partitionKey) =
+    let pushSingleEvent (eventHubClient:EventHubClient,record: 'a,partitionKey) =
         task {
-            let eventHubClient = getEventHubClient "EventHubSASConnectionString"
             let messageAsText = Newtonsoft.Json.JsonConvert.SerializeObject record
             use event = new EventData(System.Text.Encoding.UTF8.GetBytes(messageAsText))
             do! eventHubClient.SendAsync(event, partitionKey)
         }
-    let pushEvent (data: 'a,fileWriterInfo) =
+    let pushEvent (eventHubClient:EventHubClient,data: 'a,fileWriterInfo) =
         task {
             try
                 Log.logStarting ("UploadingEvents", LocalServer, Upload, EventHub, fileWriterInfo)
-                let eventHubClient = getEventHubClient "EventHubSASConnectionString"
                 let batch = eventHubClient.CreateBatch()
                 for x in data do
                     let messageAsText = Newtonsoft.Json.JsonConvert.SerializeObject x
