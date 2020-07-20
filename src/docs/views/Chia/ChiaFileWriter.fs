4bf6ce01cc7ef0e5722a69aa4364710be9ceb5e7
tforkmann <tforkmann@gmail.com>
2020-07-20 12:43:47 +0200

    Bumping version to 0.6.6

--- src/docs/views/Chia/ChiaFileWriter.fs
+++ src/docs/views/Chia/ChiaFileWriter.fs
@@ -16,6 +16,7 @@ let overview =
                 Html.p "You first have to set your development status and you also have to set your CompanyInitials and set a ProjectName."
                 Html.p "You also have to thing about where you want to get you logs get created. You can ether log locally or on Azure"
                 Html.p "Finally you can initilize your filewriter with `initFileWriter`"
+                Html.p "If you want to log to ApplicationInsight you additionally have to create a new Application Insight resource in Azure and set your ApplicationInsights key."
                 code """
                 open Chia.Domain.Logging
                 open Chia.Domain.Config
@@ -28,16 +29,4 @@ let overview =
                         projectName "TestChia"
                         devOption (Azure "aiKey")
                     }""" ]
-          Html.hr []
-          Bulma.content
-              [ Html.p "If you want to log to ApplicationInsight you additionally have to create a new Application Insight resource in Azure and set your ApplicationInsights key."
-                code """
-                open Chia.Domain.Logging
-                open Chia.Domain.Config
-                open Chia.FileWriter
-                let devStatus = getDevStatusFromEnv  /// Get your devStatus from you enviroment variable. For example pass in an enviroment variable in Fake --> '-e devStatus=Productive
-                let aiKey = "<InsertYourApplicationInsightsKey" ///Get this key from your app.config or from KeyFault
-                let fileWriterInfo = initFileWriter devStatus (CompanyInitials "CompanyInitials") (ProjectName "ProjectName") Local ""
-                let fileWriterInfoAzure = initFileWriter devStatus (CompanyInitials "CompanyInitials") (ProjectName "ProjectNames") Azure aiKey
-                let fileWriterInfoLocalAndAzure = initFileWriter devStatus "ProjectName" LocalAndAzure aiKey""" ]
           fixDocsView "ChiaFileWriter" false      ]
